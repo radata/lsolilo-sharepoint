1125fa6d4cff4abab0f21b0f1534d524a3d71fe8
looke.pl <looke.pl@5d739c29-e465-cb35-c5bb-9f0cea5e10b1>
2012-09-04 22:13:05 +0000

    tt files

---
+++ LS.Holiday/LS.Holiday.Core/HolidayContentTypes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using FPS.Core;
+using Microsoft.SharePoint;
+
+namespace LS.Holiday.Core
+{
+    public class HolidayContentTypes
+    {
+        public static ContentTypeInformation<Fields.HolidayFields> Holiday = new ContentTypeInformation<Fields.HolidayFields>("0x0100119F2245F9444258B7A642E6560B9EE3", "Holiday", new Fields.HolidayFields());
+        public static ContentTypeInformation<Fields.ProjectLeaderTaskFields> ProjectLeaderTask = new ContentTypeInformation<Fields.ProjectLeaderTaskFields>("0x0108010026E5350277764EBB9850F108E812F91A", "ProjectLeaderTask", new Fields.ProjectLeaderTaskFields());
+
+        public class Fields
+        {
+            public class HolidayFields
+            {
+                public FieldInformation Title = new FieldInformation(SPBuiltInFieldId.Title, "Title", string.Empty);
+
+                public FieldInformation StartDate { get { return HolidaysFields.StartDate; } }
+
+                public FieldInformation EndDate { get { return HolidaysFields.EndDate; } }
+
+                public FieldInformation HolidayDescription { get { return HolidaysFields.HolidayDescription; } }
+
+                public FieldInformation ProjectLeader { get { return HolidaysFields.ProjectLeader; } }
+
+                public FieldInformation Status { get { return HolidaysFields.Status; } }
+            }
+
+            public class ProjectLeaderTaskFields
+            {
+                public FieldInformation Title = new FieldInformation(SPBuiltInFieldId.Title, "Title", string.Empty);
+
+                public FieldInformation Decision { get { return HolidaysFields.Decision; } }
+
+                public FieldInformation Description { get { return HolidaysFields.HolidayDescription; } }
+            }
+        }
+    }
+}
---
+++ LS.Holiday/LS.Holiday.Core/HolidayStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LS.Holiday.Core
+{
+    public enum HolidayStatus
+    {
+        New,
+        Approved,
+        Declined
+    }
+}
